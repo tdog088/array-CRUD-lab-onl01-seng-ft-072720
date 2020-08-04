796464595597a29b447b985ca95c3204bb7ac45c
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-04 02:57:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  fruit_array = ["apple", "banana", "blueberry"]
-  fruit_array.pop
+  array = ["apple", "banana", "blueberry"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
