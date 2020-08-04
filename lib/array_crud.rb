e79410ef05e9d890153e60daffd5410de58039c8
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-04 02:54:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  fruit_array = ["apple", "banana", "blueberry"]
+  fruit_array.pop
 end
 
 def remove_element_from_end_of_array(array)
