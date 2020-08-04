813a0eafb2960562fad7d85ec34d40768acb56d4
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-04 02:51:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  sauce_array = ["ketchup", mustard]
+  sauce_array = ["ketchup", "mustard"]
   sauce_array.push("mayo")
 end
 
