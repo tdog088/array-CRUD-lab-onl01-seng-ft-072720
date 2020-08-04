6911eae103f4c74d072e74de7b3fec31b141ca96
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-04 03:08:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["I", "am", "good"]
+  array.[1]
 end
 
 def retrieve_first_element_from_array(array)
