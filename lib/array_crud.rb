3f0c1842d9a03f581de24d70792529875dba8562
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-04 02:53:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   sauce_array = ["ketchup", "mustard"]
-  sauce_array << "mayo"
+  element = "mayo"
+  sauce_array << element
 end
 
 def add_element_to_start_of_array(array, element)
